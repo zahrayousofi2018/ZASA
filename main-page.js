6cb35b30b557d56d36d952288d0bf0a61322b631
Zahra Yousofi <zahra.yousofi.2010@gmail.com>
2022-01-12 20:49:12 -0500

    commit

--- main-page.js
+++ main-page.js
@@ -4,4 +4,9 @@ const headline = document.querySelector('.headline');
 
 const tl = new TimelineMax();
 
-tl.fromTo(hero,1, {height: "0%"}, {height: "80%"})
+
+tl.fromTo
+(hero, 1, {height: "0%"},{height: "100%", ease: Power2.easeInOut })
+// .fromTo(hero, 1.2, {width: '100%'}, {width: '80%', ease: Power2.easeInOut })
+// .fromTo(slider, 1.2, {x: '-100%'}, {x: "0%", ease: Power2.easeInOut})
+
